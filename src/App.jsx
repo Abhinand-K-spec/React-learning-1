d8116eb824c7d0590a47ec4e4876f1bbbab13220
Abhinand k <abhinnanndk@gmail.com>
2026-02-05 13:07:22 +0530

    onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Card from './card'
 import profilePic from './assets/profilePic.jpeg';
-import Counter from './counter'
+import Counter from './counter';
+import Input from './onchange';
 
 
 function App() {
   return(
       <>
+        <Input/>
         <Counter/>
         <Card profilePic={profilePic} name="abhi" bio="photographer"/>
       </>
---
+++ src/onchange.jsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react';
+
+function input(){
+
+
+    const [name,setName] = useState('Guest');
+    const [payment, setPayment] = useState('');
+    const [shipping, setShipping] = useState('nothing');
+
+    function updatePayment(event){
+        setPayment(event.target.value);
+    }
+
+    function updateName(e){
+        setName(e.target.value)
+    }
+
+    function updateRadio(event){
+        setShipping(event.target.value);
+    }
+
+    return(
+        <div>
+            <input type="text" value={name} onChange={updateName}/>
+            <h1>Name : {name}</h1>
+
+            <select value={payment} onChange={updatePayment} id="">
+                <option value="select one">select one</option>
+                <option value="UPI">UPI</option>
+                <option value="Card">CARD</option>
+            </select>
+            <h1>payment:{payment}</h1>
+
+
+            <label htmlFor="">
+                <input type='radio' value="pick-up"  checked={shipping==="pick-up"} onChange={updateRadio}/>
+                Pick Up</label><br />
+            <label htmlFor="">
+            <input type='radio' value="dlv"  checked={shipping==="dlv"} onChange={updateRadio}/>
+                Delivary</label>
+
+                <h2>Status:{shipping}</h2>
+        </div>
+    );
+}
+
+
+export default input;
