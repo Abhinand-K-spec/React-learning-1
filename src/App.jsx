d283e1cbed5b86f33a136889587e5a95d1cced5f
Abhinand k <abhinnanndk@gmail.com>
2026-02-19 15:40:38 +0530

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // import Card from './card'
 // import profilePic from './assets/profilePic.jpeg';
-// import Counter from './counter';
+// import Counter from './counterReducer';
 // import Input from './onchange';
 // import Ref from './ref'
-import Communication from './childToParent';
+// import Communication from './childToParent';
 // import Calc from './calc';
 // import CounterReducer from './counterRed.jsx';
 // import Calculator from './calculatorWithReducer.jsx';
 // import List from './addToList.jsx';
 // import Timer from './timer';
-// import Products from "./fetch";
+import Products from "./fetch";
 
 function App() {
   return(
       <>
-      {/* <Products/> */}
-      <Communication/>
+      <Products/>
+      {/* <Communication/> */}
         {/* <Input/> */}
         {/* <Ref/> */}
-        {/* <Counter/> */}
+        <Counter/>
         {/* <Card profilePic={profilePic} name="abhi" bio="photographer"/> */}
       </>
   );
